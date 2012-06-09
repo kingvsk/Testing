004740aee67e5594cc3f9e1c09e1efd78aa0460e
kingvsk <>
2012-06-09 18:26:04 +0530

    First commit

---
+++ Testing/src/in/king/test.java
@@ -0,0 +1,16 @@
+package in.king;
+
+/**
+ *
+ * @author Sathishkumar Varatharasu
+ */
+public class test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Test printing...
+        System.out.println("This is Testing");
+    }
+}
